1e523b4c4c5bb356149633506dcd85f27025e551
Patrick Drechsler <patrick@pdrechsler.de>
2020-09-25 18:20:13 +0200

    statebuilder: failing Delay and Run methods

--- practice/Practice/Practice/StateBuilder.fs
+++ practice/Practice/Practice/StateBuilder.fs
@@ -42,6 +42,10 @@ type StateBuilder () =
     member __.Return(value) : State<'a, 's> = State.result value
     member __.Bind(m: State<'a, 's>, f: 'a -> State<'b, 's>) : State<'b, 's> = State.bind f m
     member __.ReturnFrom(m: State<'a, 's>) = m
+    member __.Zero() = State.result ()
+//    member __.Delay(f) = f
+//    member __.Delay(f) = State.bind f (State.result ())
+//    member __.Run(f) = f()
 
 let state = StateBuilder ()
 
@@ -96,5 +100,14 @@ let tests =
             let actual = State.exec c "Hello"
             Expect.equal actual "olleH" "Expected state of \"olleH\"."
         }
+        
+//        test "state supports if ... then with no else" {
+//            let c : State<unit, string> = state {
+//                if true then
+//                    printfn "Hello"
+//            }
+//            let actual = State.eval c ""
+//            Expect.equal actual () "Expected the value to be ()."
+//        }
     ]
             
