7d7cdd1be309cfd1d96df12cf23dec59eb3b09ab
Patrick Drechsler <patrick@pdrechsler.de>
2020-09-28 09:32:34 +0200

    stackBuilder: Combine, Delay and For

--- practice/Practice/Practice/Sequences.fs
+++ practice/Practice/Practice/Sequences.fs
@@ -30,9 +30,28 @@ module Stack =
     /// Pushed a value onto a new stack.
     let lift v = push v Empty
     
+    let append s1 s2 =
+        let rec loop s1 cont =
+            match s1 with
+            | Cons(top, rem) ->
+                loop rem (fun rest -> cont(Cons(top, rest)))
+            | Empty -> cont s2
+        loop s1 id
+        
+    let collect (f: 'a -> Stack<'b>) (m: Stack<'a>) =
+        let rec loop s cont =
+            match s with
+            | Cons(top, rem) ->
+                loop rem (fun rest -> cont(append (f top) rest))
+            | Empty -> cont Empty
+        loop m id
+    
 type StackBuilder() =
     member __.Yield(value) = Stack.lift value
     member __.YieldFrom(m: Stack<'a>) = m
+    member __.Combine(s1: Stack<'a>, s2: Stack<'a>) = Stack.append s1 s2
+    member __.Delay(f: unit -> Stack<'a>) = f()
+    member __.For(m, f) = Stack.collect f m
 
 let stack = StackBuilder()
 
@@ -52,6 +71,24 @@ let tests =
         test "stack can yield an empty stack" {
             let actual = stack { yield! Empty }
             Expect.equal actual Empty "Expected an empty stack"
+        }
+        
+        test "stack can return multiple items" {
+            let actual = stack {
+                yield 1
+                yield 2
+                yield 3
+            }
+            Expect.equal (Stack.toList actual) [1;2;3] "Actual should match expected"
+        }
+        
+        test "stack can iterate and yield" {
+            let expected = stack { yield 1; yield 2; yield 3 }
+            let actual = stack {
+                for x in expected do
+                    yield x
+            }
+            Expect.equal actual expected "Expected iterating and yielding to return the same result"
         }
     ]
     
