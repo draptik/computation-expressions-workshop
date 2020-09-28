e17e7bc9fd8ba9938b3227c990003c46e3c3c619
Patrick Drechsler <patrick@pdrechsler.de>
2020-09-28 12:23:55 +0200

    querybuilder: add HeadOrNone

--- practice/Practice/Practice/Queries.fs
+++ practice/Practice/Practice/Queries.fs
@@ -1,7 +1,38 @@
 module Queries
 
+//open System.Linq
+//open System.Reactive.Linq
+//open System.Reactive.Concurrency
+open Microsoft.FSharp.Linq
 open Expecto
+
+type Microsoft.FSharp.Linq.QueryBuilder with
+
+    [<CustomOperation("headOrNone")>]
+    member __.HeadOrNone(source: QuerySource<'T, 'Q>) =
+        Seq.tryHead source.Source
+
+type TestRec = { Value: int }
+
 
 [<Tests>]
 let tests =
-    testList "queries" []
+    testList "queries" [
+        test "query supports F# types with headOrDefault" {
+            let actual : TestRec =
+                query {
+                    for x in Seq.empty<TestRec> do
+                    headOrDefault
+                }
+            Expect.equal actual (Unchecked.defaultof<TestRec>) "Expected default value of TestRec"
+        }
+        
+        test "query supports F# types with headOrNone" {
+            let actual =
+                query {
+                    for x in Seq.empty<TestRec> do
+                    headOrNone
+                }
+            Expect.equal actual None "Expected default value of TestRec"
+        }
+    ]
