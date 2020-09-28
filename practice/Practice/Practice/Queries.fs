7546923e6d3e3286cfdea3c67cb0de79555535b4
Patrick Drechsler <patrick@pdrechsler.de>
2020-09-28 12:27:40 +0200

    querybuilder: add ExactlyOneOrNone

--- practice/Practice/Practice/Queries.fs
+++ practice/Practice/Practice/Queries.fs
@@ -1,6 +1,6 @@
 module Queries
 
-//open System.Linq
+open System.Linq
 //open System.Reactive.Linq
 //open System.Reactive.Concurrency
 open Microsoft.FSharp.Linq
@@ -11,6 +11,13 @@ type Microsoft.FSharp.Linq.QueryBuilder with
     [<CustomOperation("headOrNone")>]
     member __.HeadOrNone(source: QuerySource<'T, 'Q>) =
         Seq.tryHead source.Source
+        
+    [<CustomOperation("exactlyOneOrNone")>]
+    member __.ExactlyOneOrNone(source: QuerySource<'T, 'Q>) =
+        if Seq.length source.Source = 1 then
+            Enumerable.Single(source.Source) |> Some
+        else
+            None
 
 type TestRec = { Value: int }
 
@@ -34,5 +41,35 @@ let tests =
                     headOrNone
                 }
             Expect.equal actual None "Expected default value of TestRec"
+        }
+                 
+        test "query exactlyOneOrNone returns the single value for a seq with one element" {
+            let source = seq { yield { Value = 1 } }
+            let actual =
+                query {
+                    for x in source do
+                    exactlyOneOrNone
+                }
+            Expect.equal actual (Seq.tryHead source) "Expected { Value = 1 }"
+        }
+
+        test "query exactlyOneOrNone returns None for an empty seq" {
+            let source = Seq.empty<TestRec>
+            let actual =
+                query {
+                    for x in source do
+                    exactlyOneOrNone
+                }
+            Expect.equal actual None "Expected None"
+        }
+
+        test "query exactlyOneOrNone returns None for a seq with more than one element" {
+            let source = seq { yield { Value = 1 }; yield { Value = 2 } }
+            let actual =
+                query {
+                    for x in source do
+                    exactlyOneOrNone
+                }
+            Expect.equal actual None "Expected None"
         }
     ]
