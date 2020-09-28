b37a34808694a018214a955f2057ae0aef032446
Patrick Drechsler <patrick@pdrechsler.de>
2020-09-28 13:00:00 +0200

    querybuilder: add Select to RxQuery

--- practice/Practice/Practice/Queries.fs
+++ practice/Practice/Practice/Queries.fs
@@ -1,8 +1,9 @@
 module Queries
 
+open System
 open System.Linq
-//open System.Reactive.Linq
-//open System.Reactive.Concurrency
+open System.Reactive.Linq
+open System.Reactive.Concurrency
 open Microsoft.FSharp.Linq
 open Expecto
 
@@ -18,6 +19,18 @@ type Microsoft.FSharp.Linq.QueryBuilder with
             Enumerable.Single(source.Source) |> Some
         else
             None
+
+type RxQueryBuilder() =
+    member __.For (s: IObservable<_>, body: _ -> IObservable<_>) = s.SelectMany(body)
+    member __.Yield (value) = Observable.Return(value)
+    member __.Zero () = Observable.Empty(Scheduler.CurrentThread :> IScheduler)
+    
+    [<CustomOperation("select")>]
+    member __.Select(s: IObservable<_>, [<ProjectionParameter>] selector: _ -> _) =
+        s.Select(selector)
+
+let rxquery = RxQueryBuilder()
+
 
 type TestRec = { Value: int }
 
@@ -72,4 +85,17 @@ let tests =
                 }
             Expect.equal actual None "Expected None"
         }
+        
+        test "rxquery can select values from a source" {
+            let expected = [|1..10|]
+            let actual = Array.zeroCreate<int> 10
+            let source = Observable.Range(1, 10)
+            use disp =
+                rxquery {
+                    for x in source do
+                    select x
+                }
+                |> Observable.subscribe (fun i -> actual.[i - 1] <- i)
+            Expect.equal actual expected "Expected observable to populate empty array with selected values"
+        }        
     ]
